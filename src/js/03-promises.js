8440530bd2456bd7b43b32760a38f5ae91c8d957
Christian Duda <duda.kristian.ns@gmail.com>
2022-02-13 17:27:33 +0200

    Update 03-promises.js
    finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,40 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const elems = evt.currentTarget.elements;
+
+  let delay = Number(elems.delay.value);
+  let step = Number(elems.step.value);
+  let amount = Number(elems.amount.value);
+
+  for (let position = 1; position <= amount; position++) {
+    delay += step;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`),
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`),
+      );
   }
 }
+
+formEl.addEventListener('submit', onFormSubmit);
